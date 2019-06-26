4fe2e01c377cbcb260b282b140b55d688ed1fee7
david.vodrazka <david.vodrazka@firma.seznam.cz>
2019-06-26 09:14:06 +0200

    chore: provide docstring for the functions

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import matplotlib.pyplot as plt
 import math
 
+# List that holds x and y coordinates of the two points of original line
 points = [[0, 0], [100, 0]]
+
+# How many times should the line be transformed
 number_of_iterations = 10
 
 
 def make_triangle(point_a, point_b):
+    """Return points A, B and C
+    C is the tip of a equilateral triangle constructed from middle third of original AB
+    new A and B are first and second third of original AB"""
     a_x = point_a[0] + (point_b[0] - point_a[0]) / 3
     a_y = point_a[1] + (point_b[1] - point_a[1]) / 3
     b_x = point_a[0] + 2 * ((point_b[0] - point_a[0]) / 3)
@@ -16,6 +22,7 @@ def make_triangle(point_a, point_b):
 
 
 def next_iteration(arr):
+    """Add three points between every two points in arr"""
     i = 0
     while i < len(arr) - 1:
         triangle = make_triangle(arr[i], arr[i + 1])
@@ -26,6 +33,7 @@ def next_iteration(arr):
 
 
 def draw_line(arr):
+    """Plot lines between points in arr"""
     # List to hold x values.
     x_number_values = []
 
